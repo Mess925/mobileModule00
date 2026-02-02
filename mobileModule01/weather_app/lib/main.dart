269f87fe3a09f4ed84ad3caf79c266d4f9b02cad
Mess925 <hanminthant222@gmail.com>
2026-02-02 21:30:45 +0800

    finish first one

--- mobileModule01/weather_app/lib/main.dart
+++ mobileModule01/weather_app/lib/main.dart
@@ -13,6 +13,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Currently')),
+            Center(child: Text('Today')),
+            Center(child: Text('Weekly')),
+          ],
+        ),
+        bottomNavigationBar: Container(
+          color: Colors.blue,
+          child: const TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white70,
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(icon: Icon(Icons.access_time), text: 'Currently'),
+              Tab(icon: Icon(Icons.today), text: 'Today'),
+              Tab(icon: Icon(Icons.view_week), text: 'Weekly'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class Current extends StatelessWidget {
   const Current({super.key});
 
@@ -28,21 +61,19 @@ class Current extends StatelessWidget {
       ),
       actions: [
         Padding(
-          padding: EdgeInsets.all(8),
+          padding: EdgeInsets.fromLTRB(16, 8, 0, 8),
           child: VerticalDivider(color: Colors.white),
         ),
-        Icon(Icons.location_on),
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 8, 8, 8),
+          child: Icon(Icons.location_on),
+        ),
       ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: header(),
-      body: const Center(
-        child: Text('Displaying current weather information here.'),
-      ),
-    );
+    return Scaffold(appBar: header(), body: const Body());
   }
 }
